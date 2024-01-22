5aa9d065b89dad1f8d5390d88ea4894557aafd5a
Cameron Angus <cjangus@gmail.com>
2024-01-22 16:55:09 +0900

    Move build target enumeration into help class.

---
+++ build2-VS/Toolchain/BuildTargets.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using B2VS.Toolchain.Json;
+
+namespace B2VS.Toolchain
+{
+    internal static class BuildTargets
+    {
+        public static async Task<IEnumerable<Json.B.DumpLoad.BuildLoadStatus.Target>> EnumerateBuildfileTargetsAsync(string buildfilePath, CancellationToken cancellationToken)
+        {
+            // @todo: think we can invoke on the buildfile itself, but unsure of syntax so for now using the containing directory target (should do same thing)
+            var targetPath = Path.GetDirectoryName(buildfilePath) + '/';
+            var jsonDumpStr = "";
+            var stdErr = "";
+            Action<string> outputHandler = (string line) => jsonDumpStr += line;
+            Action<string> errorHandler = (string line) => stdErr += line;
+            var exitCode = await Build2Toolchain.B.InvokeQueuedAsync(
+                // @NOTE: --dump-scope=x, if x is relative it appears to be interpreted relative to cwd, not path of the given target
+                new string[] { "--load-only", "--dump=load", string.Format("--dump-scope={0}", targetPath), "--dump-format=json-v0.1", targetPath },
+                cancellationToken,
+                outputHandler,
+                errorHandler);
+            if (exitCode != 0)
+            {
+                throw new Exception(string.Format("'b --dump' failed: {0}", stdErr));
+            }
+
+            var json = JsonSerializer.Deserialize<Json.B.DumpLoad.BuildLoadStatus>(jsonDumpStr);
+            if (json == null)
+            {
+                throw new Exception("'b --dump' json output not parseable");
+            }
+
+            return (IEnumerable<Json.B.DumpLoad.BuildLoadStatus.Target>)json.targets;
+        }
+    }
+}
